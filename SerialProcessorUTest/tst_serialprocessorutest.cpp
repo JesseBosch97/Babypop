9fecbe6e7bd2b2193f8f1361ba7edd26ad77bb9c
JesseBosch97 <J.Bosch2@student.han.nl>
2022-02-22 12:22:47 +0100

    cleaned up tests

--- SerialProcessorUTest/tst_serialprocessorutest.cpp
+++ SerialProcessorUTest/tst_serialprocessorutest.cpp
@@ -9,6 +9,7 @@
 
 // add necessary includes here
 #define THRESHOLD 100;
+#define CompleteInputSampleinChar '[', '1', '0', '2', '0', ',', ' ', '1', '0', '2', '0', ',', ' ', '1', '0', '2', '0', ',', ' ', '1', '0', '2', '0', ',', ' ', '1', '4', ' ', '1', '3', ' ', '1', '3', ' ', '1', '3', ']'
 
 class SerialProcessorUTest : public QObject
 {
@@ -18,8 +19,17 @@ public:
     SerialProcessorUTest();
     ~SerialProcessorUTest();
 
-    int handleInput(std::string inputSample){
-        return 0;
+    int handleInput(std::string inputSample)
+    {
+        if(inputSample != "")
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+
     }
 
     std::vector<std::string> StringDesect(std::string inputSample)
@@ -30,7 +40,7 @@ public:
         int stringlength = inputSample.length();
         char stringDesectedtoChars_array[stringlength + 1];
 
-        strcpy(stringDesectedtoChars_array, inputSample.c_str());
+        strncpy(stringDesectedtoChars_array, inputSample.c_str(), sizeof(stringDesectedtoChars_array));
 
         for(int i = 0; i < (int)inputSample.length(); i++)
         {
@@ -55,15 +65,6 @@ public:
 
     }
 
-    std::string findValue(std::string inputSample){
-
-        return inputSample.substr(1, 4);
-    }
-    std::string FindValueX(std::string inputSample, int start, int size)
-    {
-        return inputSample.substr(start, size);
-    }
-
     std::string copyString(std::string input){
         std::string copy;
         for (int i = 0; i <(int)input.length(); i++){
@@ -77,26 +78,20 @@ public:
         std::vector<char> desected;
         char stringDesectedtoChars_array[teststring.length() + 1];
 
-        strcpy(stringDesectedtoChars_array, teststring.c_str());
+        strncpy(stringDesectedtoChars_array, teststring.c_str(), sizeof(stringDesectedtoChars_array));
 
         for(int i = 0; i < (int)teststring.length(); i++)
         {
             desected.push_back(stringDesectedtoChars_array[i]);
         }
-
-
         return desected;
     }
 
 private slots:
-    void InputBelowThresholdDoesNotTriggerACompression();
 
-    void InputAboveThresholdDoesTriggerACompression();
-    void TheFirstValueIsFound();
-    void ValueXisFound();
-    void AllValuesAreFound();
     void StringIsCopied();
     void disect();
+    void AllValuesAreFound();
 };
 
 SerialProcessorUTest::SerialProcessorUTest()
@@ -112,19 +107,13 @@ SerialProcessorUTest::~SerialProcessorUTest()
 void SerialProcessorUTest::disect()
 {
     std::string inputSample = "[1020, 1020, 1020, 1020, 14, 13, 13, 13]\r\n";
-    std::vector<char> inputSampleinChar {'[', '1', '0', '2', '0',};
+    std::vector<char> inputSampleinChar {CompleteInputSampleinChar};
 
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < (int)sizeof(inputSampleinChar); i++)
     {
         QCOMPARE(disecttochars(inputSample)[i], inputSampleinChar[i]);
 
     }
-}
-void SerialProcessorUTest::TheFirstValueIsFound()
-{
-    std::string inputSample = "[1020, 1020, 1020, 1020, 14, 13, 13, 13]\r\n";
-    std::string subString = findValue(inputSample);
-    QCOMPARE(subString, "1020");
 }
 
 void SerialProcessorUTest::StringIsCopied()
@@ -144,48 +133,6 @@ void SerialProcessorUTest::AllValuesAreFound()
     {
         QCOMPARE(StringDesect(inputSample)[i], inputSampledesect[i]);
     }
-}
-
-void SerialProcessorUTest::ValueXisFound()
-{
-    std::string inputSample = "[1020, 1020, 1020, 1020, 14, 13, 13, 13]\r\n";
-    int start = 1;
-    int size = 4;
-
-    for(int i = 0; i < 8; i++)
-    {
-        if(i<4)
-        {
-            std::string subString = FindValueX(inputSample, start, size);
-            QCOMPARE(subString, "1020");
-        }
-        else if(i == 4)
-        {
-            size = 2;
-            std::string subString = FindValueX(inputSample, start, size);
-            QCOMPARE(subString, "14");
-        }
-        else
-        {
-            std::string subString = FindValueX(inputSample, start, size);
-            QCOMPARE(subString, "13");
-        }
-        start = start + size + 2;
-    }
-}
-
-
-
-void SerialProcessorUTest::InputBelowThresholdDoesNotTriggerACompression()
-{
-    std::string inputSample = "[1020, 1020, 1020, 1020, 14, 13, 13, 13]\r\n";
-    QCOMPARE(handleInput(inputSample), 0);
-}
-
-void SerialProcessorUTest::InputAboveThresholdDoesTriggerACompression()
-{
-    std::string inputSample = "[1020, 1020, 1020, 1020, 101, 13, 13, 13]\r\n";
-    QCOMPARE(handleInput(inputSample), 1);
 }
 
 
