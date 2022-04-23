90bdd66310ea21985af325329c9c8acd34e54464
nvankessel <n.vankessel3@student.han.nl>
2022-04-23 13:31:05 +0200

    added volume performance

--- CPR_Feedback_Game/PerformanceTypes.h
+++ CPR_Feedback_Game/PerformanceTypes.h
@@ -13,11 +13,19 @@ struct Fingerposition
     int bottomright;
 };
 
-struct FlowPerformance{
+struct FlowPerformance
+{
     int ventilationTime = 0;
     int pauseTime = 0;
     int averageFlowStrength = 0;
     int maxFlowStrength = 0;
+};
+
+struct VolumePerformance
+{
+    float volumeIn = 0;
+    float volumeOut = 0;
+    int ventilationTime = 0;
 };
 
 #endif // PERFORMANCETYPES_H
